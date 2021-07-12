fd3df344e5495e28ca8a1b782a7f80e4b1072e51
ec-ayrton <ec.ayrton@gmail.com>
2021-07-11 23:41:39 -0300

    gerenciamento de exceptions e findbyid no client

--- src/main/java/com/ayrton/project/services/ClientService.java
+++ src/main/java/com/ayrton/project/services/ClientService.java
@@ -1,12 +1,14 @@
 package com.ayrton.project.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ayrton.project.entities.Client;
 import com.ayrton.project.repositories.ClientRepository;
+import com.ayrton.project.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -16,5 +18,9 @@ public class ClientService {
 	
 	public List<Client> findAll(){
 		return repository.findAll();
+	}
+	public Client findById(Long id) {
+		Optional<Client> c = repository.findById(id);
+		return c.orElseThrow( ()-> new ResourceNotFoundException(id));
 	}
 }
---
+++ src/main/java/com/ayrton/project/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.ayrton.project.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. id: "+id);
+	}
+}
