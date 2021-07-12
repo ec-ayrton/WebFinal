44264631700f5dce72e04b3ad0a4fdcb62174e47
ec-ayrton <ec.ayrton@gmail.com>
2021-07-11 21:26:09 -0300

    profile test e h2database

--- src/main/java/com/ayrton/project/entities/Client.java
+++ src/main/java/com/ayrton/project/entities/Client.java
@@ -3,12 +3,19 @@ package com.ayrton.project.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Client implements Serializable{
-	/**
-	 * 
-	 */
+	
+	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String CPF;
