95a2f4ee370a466ee680c3f33b2aaed1f7bb547a
ec-ayrton <ec.ayrton@gmail.com>
2021-07-15 13:51:37 -0300

    implementacao dos metodos subtotal e total

--- src/main/java/com/ayrton/project/entities/Order.java
+++ src/main/java/com/ayrton/project/entities/Order.java
@@ -70,6 +70,14 @@ public class Order implements Serializable{
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+	public Double getTotal() {
+		double total = 0.0;
+		for(OrderItem x: items) {
+			total = total + x.getSubTotal();
+		}
+		return total;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(client, id);
